fbe9bd5310bc717b9dd0d9a05e057786c7f70c74
Dillon L. Rolnick <dillon.l.rolnick@vanderbilt.edu>
2017-04-26 13:21:13 -0500

    added TODO's

--- 1_game_picker.py
+++ 1_game_picker.py
@@ -16,11 +16,23 @@ def run_classifier(clf, X, Y):
         prediction = clf.predict_proba(X[test])
         aucs.append(roc_auc_score(Y[test], prediction[:, 1]))
     name, aucs, mean_auc = clf.__class__.__name__, aucs, numpy.mean(aucs)
+    #print Area-Under-the-Curve information
     print name, aucs, mean_auc
     return name, aucs, mean_auc
 
+def extract_labels(Arr):
+    #TODO take the labels out of the downloaded CSV, return an array of labels
+
+def extract_metrics (Arr):
+    #TODO extract data that we think will be sufficiently predictive
+    # Return 2D array of statistics
+    # Data rows should correspond to games and labels, columns should be useful metrics
+
+def load_data:
+    #TODO Extract data from CSV and put in 2D array
 
 def main ():
+    #TODO Pick a type of classifier, run classifier on extracted data   
     run_classifier(clf, X, Y)
 
 if __name__ == '__main__':
